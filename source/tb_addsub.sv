2373d3b639efea5e59974f468d3fe1141cbbe2c9
Andy Shi <shi110@purdue.edu>
2015-03-28 20:17:04 -0400

    finished multiply, added flags

---
+++ source/tb_addsub.sv
@@ -0,0 +1,13 @@
+// $Id: $
+// File name:   tb_addsub.sv
+// Created:     3/28/2015
+// Author:      Andy Shi
+// Lab Section: 337-03
+// Version:     1.0  Initial Design Entry
+// Description: test bench for add/sub
+`timescale 1ns/10ps
+
+module tb_addsub();
+
+localparam	CLK_PERIOD	= 2.5;
+localparam	CHECK_DELAY = 1; 
