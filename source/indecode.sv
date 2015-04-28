8d7935c84a1dfdfcc62aa3d1797d0a723fcbb94f
Matt King <king133@purdue.edu>
2015-04-28 19:56:30 -0400

    updated indecode to remove edge detecting strobe, as well as improved readability of code

--- source/indecode.sv
+++ source/indecode.sv
@@ -1,28 +1,25 @@
-// $Id: $
-// File name:   indecode.sv
-// Created:     3/24/2015
 // Author:      Matt King
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
 // Description: operation decoder 
 
 module indecode(
-input wire clk,
-input wire n_rst,
-input wire op_strobe,
-input reg [31:0] op1,
-input reg [31:0] op2,
-input reg [2:0] op_sel,
-input wire out_fifo_hold,
-input wire add_busy, mul_busy, sine_busy,
-output reg [31:0] op1_out,
-output reg [31:0] op2_out,
-output reg add_start, mul_start, sine_start, cpu_hold, [2:0]opcode_out
+  input wire clk, //system clock
+  input wire n_rst, //system reset
+  input wire op_strobe, //data valid write to fifo
+  input reg [31:0] op1, //operand 1
+  input reg [31:0] op2, //operand 2
+  input reg [2:0] op_sel, //operation select
+  input wire add_busy, mul_busy, sine_busy, //operation busy signals
+  input wire out_fifo_hold, //hold signal from output fifo
+  output reg [31:0] op1_out, //operand output 1 to operation block
+  output reg [31:0] op2_out, //operand output 2 to operation block
+  output reg add_start, mul_start, sine_start, //operation start signals
+  output reg cpu_hold, //fifo full signal
+  output reg [2:0] opcode_out
 );
-reg strobe_reg;
-reg prev_strobe1;
-reg prev_strobe2;
-reg strobe_edge;
+
+//fifo registers
 reg [66:0]reg0;
 reg [66:0]reg1;
 reg [66:0]reg2;
@@ -39,35 +36,39 @@ reg [66:0]next_reg4;
 reg [66:0]next_reg5;
 reg [66:0]next_reg6;
 reg [66:0]next_reg7;
-reg [66:0]data_out;
-reg [66:0]next_data;
+
+reg [2:0]fifo_status;
+reg prev_pop;
+reg fifo_pop;
+reg pop;
 
 //fifo pointers
 reg [2:0]in_point;
 reg [2:0]out_point;
-//reg [2:0]next_in_point;
-//reg [2:0]next_out_point;
-reg [2:0]fifo_status;
-reg fifo_pop;
-reg prev_pop;
-reg pop;
-assign cpu_hold = out_fifo_hold || (fifo_status == 7);
-assign op1_out = data_out[66:35];
-assign op2_out = data_out[34:3];
-assign opcode_out = data_out[2:0];
 
-//pointer updating
+//data output registers
+reg [66:0]data_out;
+reg [66:0]next_data;
+
+//output signal assignments
+assign cpu_hold = out_fifo_hold || (fifo_status == 7);
+assign op1_out = data_out[66:35]; //first 32 bits
+assign op2_out = data_out[34:3]; //next 32 bits
+assign opcode_out = data_out[2:0]; //last 3 bits
+
+//update in pointer when pushing new frame (strobe signal asserted)
 always @ (posedge clk, negedge n_rst)
 begin
   if(~n_rst) begin
     in_point <= 0;
-  end else if (strobe_edge) begin
+  end else if(op_strobe) begin
     in_point <= in_point + 1;
-  end else begin
+  end else begin 
     in_point <= in_point;
   end
 end
 
+//update out pointer when popping new frame (pop signal asserted)
 always @ (posedge clk, negedge n_rst)
 begin
   if(~n_rst) begin
@@ -79,14 +80,14 @@ begin
   end
 end
 
+//determine fifo status (number of frames currently in buffer)
 always @ (posedge clk, negedge n_rst)
 begin
   if(~n_rst) begin
     fifo_status <= 0;
-  end
-  else if(strobe_edge && !fifo_pop) begin
+  end else if(op_strobe && !fifo_pop) begin
     fifo_status <= fifo_status + 1;
-  end else if(fifo_pop && !strobe_edge) begin
+  end else if(fifo_pop && !op_strobe) begin
     if(fifo_status == 0) begin
       fifo_status <= 0;
     end else begin
@@ -97,19 +98,7 @@ begin
   end
 end
 
-always @ (posedge clk, negedge n_rst)
-begin
-  if(~n_rst) begin
-    strobe_reg <= 0;
-    prev_strobe1 <= 0;
-    prev_strobe2 <= 0;
-  end else begin
-    strobe_reg <= strobe_edge;
-    prev_strobe1 <= op_strobe;
-    prev_strobe2 <= prev_strobe1;
-  end
-end
-
+//state machine logic for fifo registers
 always @ (posedge clk, negedge n_rst)
 begin
   //active low reset all regs 
@@ -139,64 +128,54 @@ begin
   end
 end
 
-//determine next reg values
 always_comb
 begin
-  if(in_point == 0 && strobe_edge) begin 
+  if(in_point == 0 && op_strobe) begin 
     next_reg0 = {op1,op2,op_sel};
   end else begin
     next_reg0 = reg0;
   end
 
-  if(in_point == 1 && strobe_edge) begin
+  if(in_point == 1 && op_strobe) begin
     next_reg1 = {op1,op2,op_sel};
   end else begin
     next_reg1 = reg1;
   end
        
-  if(in_point == 2 && strobe_edge) begin
+  if(in_point == 2 && op_strobe) begin
     next_reg2 = {op1,op2,op_sel};
   end else begin
     next_reg2 = reg2;
   end
 
-  if(in_point == 3 && strobe_edge) begin
+  if(in_point == 3 && op_strobe) begin
     next_reg3 = {op1,op2,op_sel};
   end else begin
     next_reg3 = reg3;
   end
   
-  if(in_point == 4 && strobe_edge) begin
+  if(in_point == 4 && op_strobe) begin
     next_reg4 = {op1,op2,op_sel};
   end else begin 
     next_reg4 = reg4;
   end
 
-  if(in_point == 5 && strobe_edge) begin
+  if(in_point == 5 && op_strobe) begin
     next_reg5 = {op1,op2,op_sel};
   end else begin
     next_reg5 = reg5;
   end
 
-  if(in_point == 6 && strobe_edge) begin
+  if(in_point == 6 && op_strobe) begin
     next_reg6 = {op1,op2,op_sel};
   end else begin
     next_reg6 = reg6;
   end
 
-  if(in_point == 7 && strobe_edge) begin
+  if(in_point == 7 && op_strobe) begin
     next_reg7 = {op1,op2,op_sel};
   end else begin
     next_reg7 = reg7;
-  end
-end
-
-always_comb
-begin
-  if(prev_strobe1 && ~prev_strobe2) begin
-    strobe_edge = 1;
-  end else begin
-    strobe_edge = 0;
   end
 end
 
